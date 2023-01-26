6c30be2959efac83b9617d94c5f9244dc1cc2d34
NoobdaPro <98887725+NoobdaPro@users.noreply.github.com>
2023-01-26 21:55:14 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -4,7 +4,7 @@ then
   git clone https://github.com/NoobdaPro/Adv-EvaMaria-Now /Adv-EvaMaria-Now
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /Adv-EvaMaria-Now
 fi
 cd /EvaMaria
 pip3 install -U -r requirements.txt
